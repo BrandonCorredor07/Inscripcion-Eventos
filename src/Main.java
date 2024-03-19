4c99bc3986709f80a33e02244c2e084a44dc6757
BrandonCorredor07 <brandon.corredor@uptc.edu.co>
2024-03-19 09:23:48 -0500

    Back Inscripciones 1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
+
+
 
         System.out.printf("Hello and welcome!");
-
+        System.out.printf("");
 
         for (int i = 1; i <= 5; i++) {
 
